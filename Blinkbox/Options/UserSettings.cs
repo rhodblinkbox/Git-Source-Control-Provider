a3c2deb8dba1ddcea33e7a7a172b0cdb9c2c3209
rhod williams <rhod@blinkbox.com>
2012-09-18 12:27:30 +0100

    add user options file

---
+++ Blinkbox/Options/UserSettings.cs
@@ -0,0 +1,55 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="DeploySettings.cs" company="blinkbox">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace GitScc.Blinkbox.Options
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public class UserSettings : SettingsBase
+    {
+        /// <summary>
+        /// private instance of BlinkboxSccOptions.
+        /// </summary>
+        private static UserSettings sccOptions;
+
+        public bool? PreviewGetLatest { get; set; }
+        public bool? OpenUrlsInVS { get; set; }
+        
+        /// <summary>
+        /// Gets Current.
+        /// </summary>
+        public static UserSettings Current
+        {
+            get
+            {
+                if (sccOptions == null)
+                {
+                    string configFileName = Path.Combine(Environment.SpecialFolder.ApplicationData.ToString(), "blinkboxScc.settings");
+                    sccOptions = SettingsBase.LoadFromConfig<UserSettings>(configFileName);
+                }
+
+                return sccOptions;
+            }
+        }
+
+        /// <summary>
+        /// Inits this instance.
+        /// </summary>
+        protected override void Init()
+        {
+            this.PreviewGetLatest = this.PreviewGetLatest ?? false;
+            this.OpenUrlsInVS = this.OpenUrlsInVS ?? false;
+        }
+    }
+}
